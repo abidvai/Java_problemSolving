15ea6b458842bac6f94dcc12303b393d0bf32a5c
Abdullah Al Abid <abdullahabid1660@gmail.com>
2023-07-13 19:17:33 +0600

    Add files via upload

---
+++ Patterns/type_14.java
@@ -0,0 +1,16 @@
+package Patterns;
+import java.util.*;
+
+public class type_14 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=0; i < n; i++) {
+			for(char ch = 'A'; ch <= 'A' + i; ch++) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+	}
+}
